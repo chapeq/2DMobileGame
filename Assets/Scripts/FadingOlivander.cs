385f3514c9c86a6284804bca72181956a68c7628
Charlotte Pequignot <pequignotcharlotte@gmail.com>
2020-11-12 04:43:53 +0100

    Update Level1

---
+++ Assets/Scripts/FadingOlivander.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions.Must;
+using UnityEngine.Tilemaps;
+
+public class FadingOlivander : MonoBehaviour
+{
+    public GameObject TileMapFade1;
+    public GameObject TileMapFade2;
+
+    private bool IsFade = false;
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            IsFade = !IsFade;
+            if (IsFade)
+            {
+                TileMapFade1.GetComponent<TilemapRenderer>().enabled = false;
+                TileMapFade2.GetComponent<TilemapRenderer>().enabled = false;
+                TileMapFade1.GetComponent<TilemapCollider2D>().enabled = false;
+                TileMapFade2.GetComponent<TilemapCollider2D>().enabled = false;
+            }
+            else
+            {
+                TileMapFade1.GetComponent<TilemapRenderer>().enabled = true;
+                TileMapFade2.GetComponent<TilemapRenderer>().enabled = true;
+                TileMapFade1.GetComponent<TilemapCollider2D>().enabled = true;
+                TileMapFade2.GetComponent<TilemapCollider2D>().enabled = true;
+            }
+        }
+
+    }
+
+   
+
+       
+    
+
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     public Animator animator;
 
     private Rigidbody2D rb2d;        //Store a reference to the Rigidbody2D component required to use 2D Physics.
+    Vector2 movement;
 
     // Use this for initialization
     void Start()
@@ -20,19 +21,15 @@ public class PlayerController : MonoBehaviour
     //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
     void FixedUpdate()
     {
-        //Store the current horizontal input in the float moveHorizontal.
-        float moveHorizontal = Input.GetAxis("Horizontal");
+        //Store the current horizontal input in the float moveHorizontal.   
+        movement.x= Input.GetAxis("Horizontal");
 
         //Store the current vertical input in the float moveVertical.
-        float moveVertical = Input.GetAxis("Vertical");
+         movement.y = Input.GetAxis("Vertical");
 
-        //Use the two store floats to create a new Vector2 variable movement.
-        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
-
-        animator.SetFloat("Horizontal", moveHorizontal);
-        animator.SetFloat("Vertical", moveVertical);
-        float playerSpeed = Math.Abs(rb2d.velocity.x);
-        animator.SetFloat("Speed", playerSpeed);
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
         //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
         rb2d.MovePosition(rb2d.position + movement * speed * Time.fixedDeltaTime);
     }
